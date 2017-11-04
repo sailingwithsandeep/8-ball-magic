999873cb39ee7b92f8bb138cac97b5e06debe9c4
londonappbrewery <londonappbrewery@gmail.com>
2017-11-04 10:34:58 +0000

    Fixing Missing SDK

---
+++ app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
@@ -0,0 +1,14 @@
+package com.londonappbrewery.magiceightball;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+public class ExampleInstrumentedTest extends ApplicationTestCase<Application> {
+    public ExampleInstrumentedTest() {
+        super(Application.class);
+    }
+}
